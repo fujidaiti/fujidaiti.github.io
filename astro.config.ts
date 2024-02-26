c9423ae17c5e11fb6a0319806c7ec6b4e830d91f
Daichi Fujita <68946713+fujidaiti@users.noreply.github.com>
2024-02-27 02:07:58 +0900

    ExpressiveCodeを導入 (#1)
    * expressive-codeをインストール
    
    * エディタの見た目を調整

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,19 @@
+import expressiveCode from 'astro-expressive-code'
 import { defineConfig } from 'astro/config'
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://fujidaiti.github.io',
+  integrations: [
+    expressiveCode({
+      themes: ['light-plus'],
+      styleOverrides: {
+        borderRadius: '0rem',
+        codeFontSize: '0.9rem',
+        frames: {
+          shadowColor: 'transparent',
+        },
+      },
+    }),
+  ],
 })
